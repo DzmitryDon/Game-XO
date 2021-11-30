6974e1b14b1692344aea3f4962b3b9235a86e696
Dzmitry Aliashkevich <dzmitrydon@gmail.com>
2021-11-30 14:30:57 +0300

    reset buttom

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,12 +5,19 @@ import { checkWinner } from "../calculationes/calculate";
 import "../styleslist/Game.css";
 
 export const Game = () => {
-  const [game, setGame] = useState([Array(9).fill(null)]);
+  const gameStart = [Array(9).fill(null)];
+
+  const [game, setGame] = useState(gameStart);
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
 
   const winner = checkWinner(game[stepNumber]);
 
+  const handleClickReset = () => {
+    setGame(gameStart);
+    setStepNumber(0);
+    setXIsNext(true);
+  };
   const handleClick = (i) => {
     const timeInGame = game.slice(0, stepNumber + 1);
     const current = timeInGame[stepNumber];
@@ -18,7 +25,7 @@ export const Game = () => {
 
     if (winner || squares[i]) return;
 
-    squares[i] = xIsNext ? "X" : "O";
+    squares[i] = xIsNext ? "X" : "0";
 
     setGame([...timeInGame, squares]);
     setStepNumber(timeInGame.length);
@@ -27,9 +34,18 @@ export const Game = () => {
 
   return (
     <div className="game-div">
-      <div className="active-player">X</div>
+      <button onClick={handleClickReset} className="reset-buttom">
+        Reset Game
+      </button>
+      <div className={xIsNext ? "active-player" : "wait-player"}>
+        <span> X</span>
+        <span className={"winners"}>{winner === "X" ? "Wins" : null}</span>
+      </div>
       <Board onClick={handleClick} squares={game[stepNumber]} />
-      <div className="wait-player">0</div>
+      <div className={xIsNext ? "wait-player" : "active-player"}>
+        <span> 0</span>
+        <span className={"winners"}>{winner === "0" ? "Wins" : null}</span>
+      </div>
     </div>
   );
 };
